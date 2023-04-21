74c3bfaff85ed48e075ca1d2bfe4fee76f6fa1d8
FaraAJ <farrukh.fa99@gmail.com>
2023-04-21 13:11:31 -0500

    Added test file.

--- test_account.py
+++ test_account.py
@@ -1,33 +1,40 @@
-import pytest
+from pytest import *
 from account import Account
 
-def test_init():
-    acc = Account("John")
-    assert acc.get_name() == "John"
-    assert acc.get_balance() == 0
+class Test:
+    def setup_method(self):
+        self.acc = Account('John')
 
-def test_deposit():
-    acc = Account("John")
-    assert acc.deposit(-100) is False
-    assert acc.deposit(0) is False
-    assert acc.deposit(100) is True
-    assert acc.get_balance() == 100
+    def teardown_method(self):
+        del self.acc
 
-def test_withdraw():
+    def test_init(self):
+        assert self.acc.get_name() == "John"
+        assert self.acc.get_balance() == 0
 
-    acc = Account("John")
-    assert acc.deposit(100) is True
 
-    assert acc.get_balance() == 100
+    def test_deposit(self):
+        assert self.acc.deposit(-100) is False
+        assert self.acc.get_balance() == 0
 
-    assert acc.withdraw(50) is True
-    assert acc.get_balance() == 50
+        assert self.acc.deposit(0) is False
+        assert self.acc.get_balance() == 0
 
-    assert acc.withdraw(-20) is False
-    assert acc.get_balance() == 50
+        assert self.acc.deposit(100) is True
+        assert self.acc.get_balance() == 100
 
-    assert acc.withdraw(100) is False
-    assert acc.get_balance() == 50
+    def test_withdraw(self):
+        assert self.acc.deposit(100) is True
+        assert self.acc.get_balance() == 100
 
-    assert acc.withdraw(0) is False
-    assert acc.get_balance() == 50
+        assert self.acc.withdraw(-20) is False
+        assert self.acc.get_balance() == 100
+
+        assert self.acc.withdraw(0) is False
+        assert self.acc.get_balance() == 100
+
+        assert self.acc.withdraw(500) is False
+        assert self.acc.get_balance() == 100
+
+        assert self.acc.withdraw(50) is True
+        assert self.acc.get_balance() == 50
